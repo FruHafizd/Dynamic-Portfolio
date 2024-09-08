7e75f74913ccab2af3b089dc442c4f7ff60e2da6
FruHafizd <muhammadhafidz12333@gmail.com>
2024-09-08 21:46:47 +0800

    Revert  assets Vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 
-                'resources/js/app.js',
-                'public/css/filament/forms/forms.css', 
-                'public/css/filament/support/support.css',
-                'public/css/filament/filament/app.css',],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
